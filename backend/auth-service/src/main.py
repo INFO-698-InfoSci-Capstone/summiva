a804de64b26f0ece538a9535e06bfbd80196a483
Aksh-Konda <aksh.konda@gmail.com>
2025-03-12 09:22:22 +0530

    auth: added health check

--- backend/auth-service/src/main.py
+++ backend/auth-service/src/main.py
@@ -4,6 +4,6 @@ from api.endpoints.auth import router
 app = FastAPI(title="Authentication Service")
 app.include_router(router)
 
-@app.get("/")
+@app.get("/health")
 def health_check():
-    return {"status": "Auth Service is running!"}
+    return {"status": "ok"}
